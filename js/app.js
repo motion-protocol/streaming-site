3bd911494ffd7958ae0f2ab99c4fd1502427a8d1
Torbjörn Josefsson <torbjorn@blockchangers.com>
2019-04-24 15:24:02 +0200

    Update to support plasma
    Former-commit-id: 8c84e5e622a71133bae6aad12d0d7f49bf9358f7 [formerly dde8ee6f7eaeb16544f24fc33aa151c6514646a4 [formerly dd821bfb818adbe2630a288cf989cabf406d5e4f]]
    Former-commit-id: 57da5943daeff5708e1b42a28b280dd1ceb298bd
    Former-commit-id: 82471733fa76f0a001887b4c082f33e95d4adb5f

--- js/app.js
+++ js/app.js
@@ -8,16 +8,18 @@ const ABI = [
 ];
 
 const GOERLI_ADDRESS    = '0x1864b231f4fd1baa9f334150900fb9af6103526c';
-const RINKEBY_ADDRESS   = '0xaa5809214239995d873f9d742351cecb4dafad30';
+const RINKEBY_ADDRESS   = '0xbf972fD0b929563407C249CBB00e33B4C83d49c3';
 const LOCALHOST_ADDRESS = ''; // UPDATE
 
 const getWeb3Provider = async(web3) => {
-  if(web3) {
-    return new ethers.providers.Web3Provider(web3.currentProvider);
-  } else {
-    throw new Error('Non-Ethereum browser detected. You should consider trying MetaMask!');
-  }
+  return new ethers.providers.JsonRpcProvider('https://staging-testnet.leapdao.org/rpc');
 };
+
+console.log(typeof window.IpfsHttpClient)
+
+const ipfs = window.IpfsHttpClient('ipfs.infura.io', '5001', {protocol: 'https'});
+
+console.log('ipfs: ', ipfs);
 
 const getContract = curry((abi, address, provider) => new ethers.Contract(address, abi, provider));
 
@@ -25,41 +27,84 @@ const getVideoGoerliContract    = getContract(ABI, GOERLI_ADDRESS);
 const getVideoRinkebyContract   = getContract(ABI, RINKEBY_ADDRESS);
 const getVideoLocalhostContract = getContract(ABI, LOCALHOST_ADDRESS);
 
-const getVideos = curry(async (contract) => {
-  const totalSupply   = await contract.totalSupply();
-  const indexes       = await times(contract.tokenByIndex ,totalSupply)
-  const movieIds      = map((id) => id.toNumber(), await Promise.all(indexes));
+const getVideos = curry(async (provider) => {
+  try {
+    const unspent = await provider.send('plasma_unspent', ['', 49153]);
+    console.log('unspent: ', unspent);
 
-  const allMovies     = await map(async(tokenId) => {
-    const uri   = await contract.tokenURI(tokenId);
-    const owner = await contract.ownerOf(tokenId);
-    return { uri, owner, tokenId };
-  }, movieIds);
+    const result = await map(async(raw) => {
+      const unencodedIpfsHash   = raw.output.data;
+      console.log("raw ipfs hash# ", unencodedIpfsHash)
+      const ipfsHash            = Base58.encode(convertToUint8Array(unencodedIpfsHash));
+      console.log("ipfs hash#", ipfsHash);
+      const cid                 = `${ipfsHash}`;
+      const content             = await JSON.parse((await getMovieRepresentation(ipfs, cid)));
 
-  return await Promise.all(allMovies);
+      const tokenId = raw.output.value;
+      const color = raw.output.color;
+      const owner = raw.output.address;
+
+      content.tokenId = tokenId;
+      content.color = color;
+      content.owner =  owner;
+      return content;
+    }, unspent);
+
+    return await Promise.all(result);
+  } catch (error) {
+    console.error(error.stack);
+  }
 });
 
-const selectNetwork = async(provider) => {
-  const {name}      = await provider.getNetwork();
-  if(name === 'goerli') {
-    return await getVideoGoerliContract(provider);
-  } else if (name === 'rinkeby') {
-    return await getVideoRinkebyContract(provider);
-  } else {
-    return await getVideoLocalhostContract(provider);
-  }
+const getMovieRepresentation = (provider, cid) => {
+  return new Promise((resolve, reject) => {
+    provider.get(cid, (err, files) => {
+      if(err) { reject(err) };
+      resolve(files[0].content.toString('utf8'));
+    });
+  });
 };
 
-// TODO: whiterabbit needs to upload all the data related to an movie , poster, video
+const convertToUint8Array = (inputAsHexString) => {
+  const result = [];
+
+  const noPrefix = inputAsHexString.slice(2);
+
+  const d2h = d => (+d).toString(16);
+
+  const ALGO = '12';
+
+  const hexStringToByte = (str) => {
+    if (!str) {
+      return new Uint8Array();
+    }
+    const a = [];
+    for (let i = 0, len = str.length; i < len; i+=2) {
+      a.push(parseInt(str.substr(i,2),16));
+    }
+    return new Uint8Array(a).length;
+  };
+
+  const s = ALGO + d2h(hexStringToByte(noPrefix)) + noPrefix;
+
+  for(var i = 0; i < s.length; i+=2) {
+    result.push(parseInt(s.substring(i, i + 2), 16));
+  }
+  return Uint8Array.from(result)
+};
+
+const selectNetwork = async(provider) => {
+  return await getVideoRinkebyContract(provider);
+};
+
 const createUiModel = (movies) => R.map((movie) => {
   console.log(movie);
-  const parts = split('#', movie.uri);
   return ({
     owner: movie.owner,
     tokenid: movie.tokenId,
-    title: parts[1],
-    video: movie.uri,
-    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMTk0MTUxNV5BMl5BanBnXkFtZTgwNTY0MjAzODE@._V1_SX300.jpg'
+    title: movie.name,
+    video: movie.movie.hls,
+    poster: movie.image
   });
 }, movies);
 
@@ -127,7 +172,8 @@ var app = new Vue({
           try {
             const provider    = await getWeb3Provider(window.web3);
             const contract    = await selectNetwork(provider);
-            const movies      = await getVideos(contract);
+            const movies      = await getVideos(provider);
+            console.log(movies);
             this.videos       = concat(this.videos, createUiModel(movies));
           } catch (error) {
             console.error(error.stack);
