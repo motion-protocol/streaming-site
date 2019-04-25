45b19db3883b6a68831cacce31460fad2e1fa864
Torbjörn Josefsson <torbjorn@blockchangers.com>
2019-04-25 11:20:18 +0200

    Refactoring
    Former-commit-id: 7877159dec7ba7e6fe6868fe8795cc9402201a07 [formerly 2dd29cd9fafef4af4d0c65e630edcd16a93f869c [formerly 1674a7eabddd28e13301b637ef4fb5b813211af6]]
    Former-commit-id: fa1ad32035338f7a8cdf1047d9c7faa6719642d6
    Former-commit-id: f3e346bd2079ca4c4c3da04c96ec3d0ce7a6b139

--- js/app.js
+++ js/app.js
@@ -1,54 +1,26 @@
 const {map, curry, times, split, concat} = R;
 
-const ABI = [
-  "function tokenByIndex(uint256 index) public view returns (uint256)",
-  "function tokenURI(uint256 tokenId) external view returns (string memory)",
-  "function totalSupply() public view returns (uint256)",
-  "function ownerOf(uint256 tokenId) public view returns (address owner)"
-];
-
-const GOERLI_ADDRESS    = '0x1864b231f4fd1baa9f334150900fb9af6103526c';
 const RINKEBY_ADDRESS   = '0xbf972fD0b929563407C249CBB00e33B4C83d49c3';
-const LOCALHOST_ADDRESS = ''; // UPDATE
 
-const getWeb3Provider = async(web3) => {
-  return new ethers.providers.JsonRpcProvider('https://staging-testnet.leapdao.org/rpc');
-};
-
-console.log(typeof window.IpfsHttpClient)
+const getWeb3Provider = async () => new ethers.providers.JsonRpcProvider('https://staging-testnet.leapdao.org/rpc');
 
 const ipfs = window.IpfsHttpClient('ipfs.infura.io', '5001', {protocol: 'https'});
 
-console.log('ipfs: ', ipfs);
-
-const getContract = curry((abi, address, provider) => new ethers.Contract(address, abi, provider));
-
-const getVideoGoerliContract    = getContract(ABI, GOERLI_ADDRESS);
-const getVideoRinkebyContract   = getContract(ABI, RINKEBY_ADDRESS);
-const getVideoLocalhostContract = getContract(ABI, LOCALHOST_ADDRESS);
-
 const getVideos = curry(async (provider) => {
   try {
-    const unspent = await provider.send('plasma_unspent', ['', 49153]);
-    console.log('unspent: ', unspent);
-
+    const unspents = await provider.send('plasma_unspent', ['', 49153]);
     const result = await map(async(raw) => {
       const unencodedIpfsHash   = raw.output.data;
-      console.log("raw ipfs hash# ", unencodedIpfsHash)
-      const ipfsHash            = Base58.encode(convertToUint8Array(unencodedIpfsHash));
-      console.log("ipfs hash#", ipfsHash);
-      const cid                 = `${ipfsHash}`;
+      const cid                 = Base58.encode(convertToUint8Array(unencodedIpfsHash));
       const content             = await JSON.parse((await getMovieRepresentation(ipfs, cid)));
-
-      const tokenId = raw.output.value;
-      const color = raw.output.color;
-      const owner = raw.output.address;
-
+      const tokenId   = raw.output.value;
+      const color     = raw.output.color;
+      const owner     = raw.output.address;
       content.tokenId = tokenId;
-      content.color = color;
-      content.owner =  owner;
+      content.color   = color;
+      content.owner   = owner;
       return content;
-    }, unspent);
+    }, unspents);
 
     return await Promise.all(result);
   } catch (error) {
@@ -87,18 +59,13 @@ const convertToUint8Array = (inputAsHexString) => {
 
   const s = ALGO + d2h(hexStringToByte(noPrefix)) + noPrefix;
 
-  for(var i = 0; i < s.length; i+=2) {
+  for(let i = 0; i < s.length; i+=2) {
     result.push(parseInt(s.substring(i, i + 2), 16));
   }
   return Uint8Array.from(result)
 };
 
-const selectNetwork = async(provider) => {
-  return await getVideoRinkebyContract(provider);
-};
-
 const createUiModel = (movies) => R.map((movie) => {
-  console.log(movie);
   return ({
     owner: movie.owner,
     tokenid: movie.tokenId,
@@ -124,7 +91,6 @@ Vue.directive('init-video', {
   inserted: function(item) {
     const siblings = getSiblings(item);
     const [title, poster, owner, tokenId] = siblings;
-    console.log('sending customEvent', title, poster, owner, tokenId)
     window.dispatchEvent(new CustomEvent("wr-video-play", {detail: { title, poster, owner, tokenId }}));
     videojs(document.getElementById(item.id)).play();
   }
@@ -160,20 +126,17 @@ var app = new Vue({
         window.dispatchEvent(new CustomEvent("wr-exist-plugin",{q:'exist-plugin'}));
       },
       eventExistPlugin: function () {
-        var self = this;
-        window.addEventListener("wr-message", function (e) {
+        window.addEventListener("wr-message", (e) => {
           console.log ('whiterabbit plugin PONG' , e.detail);
-          if (e.detail.sataus == 200) self.existPluginVal = true;
-          else self.existPluginVal = false;
+          if (e.detail.sataus == 200) this.existPluginVal = true;
+          else this.existPluginVal = false;
         } ,false);
       },
       initWeb3Provider: async function () {
         window.addEventListener('load', async () => {
           try {
-            const provider    = await getWeb3Provider(window.web3);
-            const contract    = await selectNetwork(provider);
+            const provider    = await getWeb3Provider();
             const movies      = await getVideos(provider);
-            console.log(movies);
             this.videos       = concat(this.videos, createUiModel(movies));
           } catch (error) {
             console.error(error.stack);
